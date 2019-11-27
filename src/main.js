96096578936cd53879ee3979380b9458905cd705
Vincent <gphtechak@gmail.com>
2019-11-27 23:26:34 +0200

    modules added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {createSiteMenuTemplate} from './components/site-menu.js';
-import {createSortingTemplate} from './components/sorting';
+import {createSortingTemplate} from './components/sorting.js';
 import {createFilterTemplate} from './components/filter.js';
 import {createTaskTemplate} from './components/task.js';
 import {createTaskEditTemplate} from './components/task-edit.js';
